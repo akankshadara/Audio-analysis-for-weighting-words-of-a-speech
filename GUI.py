1b42b6aed55fc7fb5f21d77ee81f13116a6e5297
vdivakar <divakarv96@gmail.com>
2016-11-09 21:31:45 +0530

    updated
    added GUI

---
+++ GUI.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import tkFileDialog
+import time
+import csv
+import subprocess
+
+from Tkinter import *
+
+
+class Window:       
+	def __init__(self, master):     
+	    self.filename=""
+	    csvfile=Label(root, text="File").grid(row=1, column=0)
+	    bar=Entry(master).grid(row=1, column=1) 
+
+	    #Buttons  
+	    y=7
+	    self.cbutton= Button(root, text="OK", command=self.process_csv)
+	    y+=1
+	    self.cbutton.grid(row=10, column=3, sticky = W + E)
+	    self.bbutton= Button(root, text="Browse", command=self.browsecsv)
+	    self.bbutton.grid(row=1, column=3)
+
+	def browsecsv(self):
+	    from tkFileDialog import askopenfilename
+
+	    # Tk().withdraw() 
+	    self.filename = askopenfilename()
+	    print self.filename
+
+	def process_csv(self):
+		print "hello world"
+		#print self.filename
+		cmd = "python audio_graph_using_functions.py "+ self.filename
+		subprocess.call(cmd, shell = True)
+	    
+root = Tk()
+window=Window(root)
+root.mainloop()  
--- audio_graph_using_functions.py
+++ audio_graph_using_functions.py
@@ -1,22 +1,21 @@
 from scipy.io.wavfile import read
 import matplotlib.pyplot as plt
 from numpy import mean
+import sys
 
-input_data = read("reason_deep_learning.wav")
+filename = sys.argv[1]
+input_data = read(filename)
 audio = input_data[1]
 frequency = input_data[0]
 abs_list = abs(audio[:, 1])
-m =  mean(abs_list)
 
-print "mean = " + str(m)
+# m =  mean(abs_list)
+# print "mean = " + str(m)
 
 def to_time(num_list):
     seconds_list = []
     for i in num_list:
         seconds_list.append((i+0.0)/frequency)
-
-    # for i in range(10):
-    #     print seconds_list[i*100: (i+1)*100]
 
     grouped_list = []
     group = []
@@ -30,9 +29,14 @@ def to_time(num_list):
         if(group!=[]):
             grouped_list.append( mean(group) )
         i+=1
-    print "printing grouped list:"
-    print "length :" + str(len(grouped_list))
+    # print "printing grouped list:"
+    # print "length :" + str(len(grouped_list))
     print grouped_list
+    try:
+        import cPickle as pickle
+    except ImportError:
+        import pickle
+    pickle.dump(grouped_list, open('timelist', 'wb'))
 
 def plot_original_graph(abs_list):
     plt.plot(abs_list)
@@ -41,8 +45,7 @@ def plot_original_graph(abs_list):
     plt.title("Sample Wav")
     plt.show()
 
-def plot_cleaned_graph(abs_list):
-    val = input("enter threshold value: ")
+def plot_cleaned_graph(abs_list, input_value):
     new_l = []
     index_list = []
 
@@ -65,8 +68,38 @@ def plot_cleaned_graph(abs_list):
 def find_audio_length(ls):
     print "audio length is: " + str( (len(ls)+ 0.0) /frequency )
 
+def ask_input_gui():
+    import Tkinter as tk
+
+    class MyDialog:
+        def __init__(self, parent):
+            # top = self.top = tk.Toplevel(parent)
+            self.myLabel = tk.Label(parent, text='Enter your username below')
+            self.myLabel.pack()
+            self.myEntryBox = tk.Entry(parent)
+            self.myEntryBox.pack()
+            self.mySubmitButton = tk.Button(parent, text='Submit', command=self.send)
+            self.mySubmitButton.pack()
+
+        def send(self):
+            self.username = self.myEntryBox.get()
+
+    def onClick():
+        root = tk.Tk()
+        inputDialog = MyDialog(root)
+        root.mainloop()
+        return inputDialog.username
+
+    val = onClick()
+    return val
+
+
 find_audio_length(abs_list)
 plot_original_graph(abs_list)
-plot_cleaned_graph(abs_list)
+input_value = ask_input_gui()
+print "here"
+print input_value
+print "there"
+plot_cleaned_graph(abs_list, input_value)
 
 
